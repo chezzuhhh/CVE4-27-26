9c6b1544dc0fe107d888b32f062bc61aae356f08
Ches <94721148+chezzuhhh@users.noreply.github.com>
2026-04-27 13:09:09 -0500

    Create AccessPoint.sh for access point setup
    Add a bash script to set up an access point and mitm environment.

---
+++ AccessPoint.sh
@@ -0,0 +1,16 @@
+# Simple custom bash script to setup an access point/mitm environment
+
+#!/bin/bash
+
+sudo ip addr add 192.168.100.1/24 dev wlan0;
+sudo ip link set wlan0 up;
+sudo sysctl -w net.ipv4.ip_forward=1;
+sudo iptables -t nat -A POSTROUTING -o wlan0 -j MASQUERADE;
+
+# HTTPS redirect
+# sudo iptables -t nat -A PREROUTING -i wlan0 -p tcp --dport 80 -j REDIRECT --to-ports 8081
+# mitmproxy --mode transparent --showhost -p 8081 -k
+
+sudo systemctl restart hostapd;
+sudo systemctl restart dnsmasq;
+ip a;
